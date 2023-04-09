71c6cc84f0be4c54cb32a9626b988977095f0622
QinglinGoue <gou.qin@northeastern.edu>
2023-04-08 20:22:12 -0400

    delete a9

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
-
-
 const app = express();
 app.use(cors())
 app.use(express.json());
